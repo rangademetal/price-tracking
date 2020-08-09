62b410618995bd41ef6febb8c73a398126c75c20
rangademetal <Sad1996.09.05>
2020-08-10 00:23:39 +0300

    Óptimization

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from tracking import price
 from tracking.secret import *
 from tracking import database
-import os
 
 
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15'}
-receive_email = ['stro3andr3i@g mail.com', 'stroeandrei483@gmail.com']
+receive_email = ['stro3andr3i@gmail.com', 'stroeandrei483@gmail.com']
 tracker = []
 
 
@@ -18,5 +17,6 @@ query = con.cursor()
 query.execute('SELECT * FROM tracking')
 myresult = query.fetchall()
 for i in myresult:
-	tracker.append(tracking.price_steam(i[1]))
-tracking.send_mail(USERNAME_GMAIL, receive_email, tracking.send_price(tracker), PASSWORD_GMAIL) 
+	dicti = tracking.price_steam(i[1])
+	tracking.convert_dict_to_arr(tracker, dicti)
+tracking.send_mail(USERNAME_GMAIL, receive_email, tracking.message_email(tracker), PASSWORD_GMAIL) 
--- tracking/price.py
+++ tracking/price.py
@@ -10,25 +10,27 @@ class price:
 	def price_steam(self, url):
 		page = requests.get(url, self.header)
 		soup = BeautifulSoup(page.content, 'html.parser')
-
 		if soup.find('div', {'class': 'discount_pct'}):
 			title = soup.find('div', {'class': 'apphub_AppName'}).get_text()
 			price = soup.find('div', {'class': 'discount_final_price'}).get_text()
-			return str(url)+'\n'+str(title)+'\n'+str(price)+'\n\n'
 		else:
 			title = soup.find('div', {'class': 'apphub_AppName'}).get_text()
 			price = soup.find('div', {'class': 'game_purchase_price price'}).get_text()
 			price = price.split()
 			price = ''.join(price)
-			# price = price.replace('€', ' - Euro')
-			return str(url)+'\n'+str(title)+'\n'+str(price)+'\n\n'
-		return 'Promotion\n\n'
-
-	def send_price(self, array):
+		items = {'url': url, 'title': title, 'price':price}
+		return items.values()
+		
+	def message_email(self, array):
 		price = ''
 		for i in array:
 			price += str(i)
 		return price
+
+	def convert_dict_to_arr(self, array, dict_array):
+		for i in dict_array:
+			array.append(i+'\n')
+		array.append('\n')
 
 	def send_mail(self, sender_email, receive_email, data, password):
 		msg = EmailMessage()
@@ -36,7 +38,6 @@ class price:
 		msg['From'] = sender_email
 		msg['To'] = receive_email
 		msg.set_content(data)
-
 		with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
 			smtp.login(sender_email, password)
 			smtp.send_message(msg)
